49e13561e1a541bf3d8d95de52a85ad97266f5f2
Anzala Ajra <dang.anzala@gmail.com>
2026-01-30 21:03:10 +0700

    Add Calculate Total button to Edit Rental page

--- app/Filament/Resources/Rentals/Pages/EditRental.php
+++ app/Filament/Resources/Rentals/Pages/EditRental.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Rentals\Pages;
 
 use App\Filament\Resources\Rentals\RentalResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Notifications\Notification;
 
 class EditRental extends EditRecord
 {
@@ -15,5 +17,56 @@ class EditRental extends EditRecord
         return [
             DeleteAction::make(),
         ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('calculate')
+                ->label('Calculate Total')
+                ->icon('heroicon-m-calculator')
+                ->color('info')
+                ->action(function () {
+                    $items = $this->data['items'] ?? [];
+                    $subtotal = 0;
+
+                    foreach ($items as $key => $item) {
+                        $dailyRate = (float) ($item['daily_rate'] ?? 0);
+                        $days = (int) ($item['days'] ?? 1);
+                        $itemSubtotal = $dailyRate * $days;
+                        
+                        $this->data['items'][$key]['subtotal'] = $itemSubtotal;
+                        $subtotal += $itemSubtotal;
+                    }
+
+                    $discount = (float) ($this->data['discount'] ?? 0);
+                    $total = $subtotal - $discount;
+
+                    $this->data['subtotal'] = $subtotal;
+                    $this->data['total'] = $total;
+
+                    Notification::make()
+                        ->title('Calculated!')
+                        ->body("Subtotal: Rp " . number_format($subtotal, 0, ',', '.') . " | Total: Rp " . number_format($total, 0, ',', '.'))
+                        ->success()
+                        ->send();
+                }),
+            
+            $this->getSaveFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function afterSave(): void
+    {
+        $this->record->refresh();
+        
+        $subtotal = $this->record->items()->sum('subtotal');
+        $total = $subtotal - ($this->record->discount ?? 0);
+
+        $this->record->update([
+            'subtotal' => $subtotal,
+            'total' => $total,
+        ]);
     }
 }
